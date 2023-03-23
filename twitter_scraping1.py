a719b4bf67bcc3840a3505ea21e3c060056d05e8
Anvitha-prasad06 <101582276+Anvitha-prasad06@users.noreply.github.com>
2023-03-23 11:12:24 +0530

    Update twitter_scraping1.py

--- twitter_scraping1.py
+++ twitter_scraping1.py
@@ -1,24 +1,20 @@
 import streamlit as st
 import pandas as pd
 import datetime
-from PIL import Image
 import pymongo
 
 #DISPLAYING DATASET
 #Reading csv file via pandas and add a button to display the loaded dataset and its length
-st.header('**:blue[Twitter Scraping:]**')
+st.title('**:red[Twitter Scraping]**')
 
 df=pd.read_csv("tweets.csv")
-
 st.text('')  
 
-result=st.button('**_Click here to view a dataset_**')
+result=st.button('Click here for original dataset')
 if result:
     st.write(len(df.index))
     st.dataframe(df)
-
-#functions to convert DataFrame to CSV,JSON and DICTIONARY    
-
+    
 def convert_df_csv(df):
     return df.to_csv()
 
@@ -28,29 +24,16 @@ def convert_df_json(df):
 def convert_df_dict(df):
     return df.to_dict('records')
 
-# Adding downlaod buttons to down DF as CSV and JSON
-
 csv=convert_df_csv(df)
 Json_data=convert_df_json(df)
 
-st.write('Click the below buttons to downlod the datset as **CSV** and **JSON** files')
-
-col1,col2,col3,col4=st.columns(4)
-
-with col1:
-    st.download_button(label='Download CSV',
-                       data=csv,
-                       file_name='tweets.csv',
-                       mime='text/csv')
-with col2:
-    st.download_button(label='Download JSON',
-                       data=Json_data,
-                       file_name='tweets.json',
-                       mime='application/json')
+st.text('')
+st.header("**:green[Filter the dataset :]**")
 
 df=pd.read_csv("tweets.csv")
 
 # Defining a function to filter the data based on given hashtag
+
 def filter_hashtag(hashtag):
     if hashtag:
         filtered_hashtag = df[df['Tweet'].str.contains(hashtag)]
@@ -65,24 +48,6 @@ hashtag = st.text_input(label='Please type your hashtag in the search bar')
 
 filtered_data = filter_hashtag(hashtag)
 
-# Adding downlaod buttons to down DF as CSV and JSON
-#st.write('Click the below buttons to downlod the #hashtag filtered datset as **CSV** and **JSON** files')
-
-col1,col2,col3,col4=st.columns(4)
-with col1:    
-    if filtered_data is not None:       
-        csv = convert_df_csv(filtered_data)
-        st.download_button(label='Download CSV', data=csv, file_name='twitter.csv', mime='text/csv')
-
-with col2:
-    if filtered_data is not None:
-        json = convert_df_json(filtered_data)
-        st.download_button(label='Download JSON', data=json, file_name='twitter.json', mime='application/json')
-    
-st.text('')  
-st.text('') 
-st.text('')   
-
 df=pd.read_csv("tweets.csv")
 
 # Convert the date column to datetime
@@ -96,21 +61,10 @@ end_date = st.date_input('Enter the end date', value=df['Date'].max().date())
 end_date = pd.to_datetime(end_date).tz_localize('UTC') + pd.DateOffset(days=1)
 
 result = df[(df['Date'] >= start_date) & (df['Date'] < end_date)]
-
-#display as DataFrame
-
-st.write('Number of tweets',len(result.index))
-display=st.button('**Click here to view the filtered dataset**')
-
-if display:
-    st.write(result)
     
 # Adding downlaod buttons to down DF as CSV and JSON
-
-st.write('Click the below buttons to downlod the filtered datset as **CSV** and **JSON** files')   
-
+ 
 col1,col2,col3,col4=st.columns(4)
-
 with col1:
     if result is not None:
         csv = convert_df_csv(result)    
@@ -122,6 +76,7 @@ with col2:
         
 #getting input from user such as Database,collection, number tweets
 
+st.header("**:blue[Upload into MongoDB :]**")
 file="tweets.csv"
 
 num_tweets = st.number_input(label='Enter the number of tweets to upload', min_value=1, step=1,max_value=10000)
@@ -132,7 +87,7 @@ db_collection=st.text_input(label='Enter the collection name or new collection n
 
 #adding a button to upload the data to MongoDB
 
-db_upload=st.button(label='**_Upload to MongoDB_**')
+db_upload=st.button(label='Upload to MongoDB')
 if db_upload:
     db=link[db_name]
     Collection=db[db_collection]
